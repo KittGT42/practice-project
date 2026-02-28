cd13a34c31e40c2c16e2ddd4a70bc1c5770e615e
Dunno_Brunno <danya.mos20@gmail.com>
2026-03-01 00:40:46 +0200

    Небольшая робота над ошибками. Почему то код не хочет работать, хотя всё сделано+- правильно.

--- task1.py
+++ task1.py
@@ -1,19 +1,14 @@
-import math
-from random import choice
-
-a = int(input())
-b = int(input())
-s = a+b
-d = a-b
-d1 = a//b
-choice = input("Select: s or d or d1")
-if choice == s:
-    print(s)
-elif choice == d:
-    print(d)
-elif choice == d1:
-    print(d1)
+def calculatin (a, b, op):
+if op == "+":
+    return a+b
+elif op == "-":
+    return a-b
+elif op == "/":
+    return a//b
 else:
     result = None
     print("Ну ті і пісюн")
 print({result:"Ничего нет"})
+a = int(input())
+b = int(input())
+op = input("Выбор операции(+ - * /):")
