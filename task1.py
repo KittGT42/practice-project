ff9cf8ba78268936e7c200ae63d947aca3e58018
Dunno_Brunno <danya.mos20@gmail.com>
2026-03-03 20:04:31 +0200

    готовая версия скрипта без излишеств

--- task1.py
+++ task1.py
@@ -1,14 +1,15 @@
 def calculatin (a, b, op):
-if op == "+":
-    return a+b
-elif op == "-":
-    return a-b
-elif op == "/":
-    return a//b
-else:
-    result = None
-    print("Ну ті і пісюн")
-print({result:"Ничего нет"})
+    if op == "+":
+     return a+b
+    elif op == "-":
+     return a-b
+    elif op == "/":
+     return a//b
+    else:
+     result = None
+     print("Ну ті і пісюн")
+
 a = int(input())
 b = int(input())
 op = input("Выбор операции(+ - * /):")
+print (calculatin(a, b, op))
